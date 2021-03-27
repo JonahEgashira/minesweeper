fde62a9b6b22a1eb8bc770deec43f616ef49a126
Jonah Egashira <jonahega@gmail.com>
2021-03-27 14:13:25 +0900

    バグの修正と見た目の改善

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "bulma/css/bulma.css";
 import './index.css';
 
 function Square(props) {
-  const value = props.visible ? props.value : '';
+  let value = props.visible ? props.value : '';
+  if (value === -1) value = '☀';
   return (
     <button className="square" onClick={props.onClick}>
       {value}
@@ -40,7 +42,7 @@ class Board extends React.Component {
 }
 
 function generateBoard(squares, visible, row, col, bombCount) {
-  // Create Grid
+  // Make Grid Two Dimentional
   for (let i = 0; i < row; i++) {
     squares[i] = new Array(col).fill(0);
     visible[i] = new Array(col).fill(null);
@@ -57,7 +59,8 @@ function generateBoard(squares, visible, row, col, bombCount) {
       bombLeft--;
     }
   }
-  // Count Bombs Around the Cell
+
+  // Count Bombs 
   const dx = [1, 1, 1, 0, 0, -1, -1, -1];
   const dy = [1, 0, -1, 1, -1, 1, 0, -1];
 
@@ -78,17 +81,31 @@ function generateBoard(squares, visible, row, col, bombCount) {
   return [squares, visible];
 }
 
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">MineSweeper</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
 class Game extends React.Component {
   constructor(props) {
+    super(props);
     const row = 9;
     const col = 9;
-    const bombCount = 10;
-    super(props);
+    const bombCount = 5;
     this.state = {
       isGameOver: false,
+      isGameCleared: false,
       row: row,
       col: col,
       bombCount: bombCount,
+      revealedCount: 0,
       squares: Array(row).fill(0),
       visible: Array(row).fill(null),
     }
@@ -97,13 +114,11 @@ class Game extends React.Component {
     this.state.visible = board[1];
   }
 
-  searchEmptyCell(i, j) {
+  searchEmptyCell(i, j, squares, visible) {
     const row = this.state.row;
     const col = this.state.col;
     const dx = [1, 1, 1, 0, 0, -1, -1, -1];
     const dy = [1, 0, -1, 1, -1, 1, 0, -1];
-    const squares = this.state.squares;
-    const visible = this.state.visible;
 
     if (visible[i][j]) return;
     visible[i][j] = true;
@@ -117,7 +132,7 @@ class Game extends React.Component {
         }
         this.setState({ visible: visible });
         if (squares[nx][ny] === 0) {
-          this.searchEmptyCell(nx, ny);
+          this.searchEmptyCell(nx, ny, squares, visible);
         }
         else {
           visible[nx][ny] = true;
@@ -127,42 +142,76 @@ class Game extends React.Component {
     }
   }
 
+  revealAllCells(visible) {
+    for (let i = 0; i < this.state.row; i++) {
+      for (let j = 0; j < this.state.col; j++) {
+        visible[i][j] = true;
+      }
+    }
+    this.setState({
+      visible : visible,
+    })
+  }
+
+  countRevealed(visible) {
+    let count = 0;
+    for (let i = 0; i < this.state.row; i++) {
+      for (let j = 0; j < this.state.col; j++) {
+        if (visible[i][j]) count++;
+      }
+    }
+    let cleared = false;
+    if (count === this.state.row * this.state.col - this.state.bombCount) {
+      cleared = true;
+    }
+    if (cleared) {
+      this.revealAllCells(visible)
+    }
+    this.setState({
+      revealedCount: count,
+      isGameCleared: cleared,
+    });
+  }
+
+
   handleClick(i, j) {
     const squares = this.state.squares.slice();
     const visible = this.state.visible.slice();
 
-    // 爆弾を押しちゃった
-    if (squares[i][j] === -1) {
-      this.setState({ isGameOver: true });
-    }
-    // ゲームオーバーだったら何もしない
-    if (this.state.isGameOver) {
+    if (this.state.isGameOver || this.state.isGameCleared) {
       return;
     }
-    // 見えてるところをクリックしても何も起きない
+    // Clicked on Bomb
+    if (squares[i][j] === -1) {
+      this.revealAllCells(visible);
+      this.setState({ isGameOver: true });
+    }
+    // Do nothing when game is over
     if (visible[i][j]) {
       return;
     }
-    // カウントが0なら、周りをみえるようにする
+    // show adjacent cells
     if (squares[i][j] === 0) {
-      this.searchEmptyCell(i, j);
+      this.searchEmptyCell(i, j, squares, visible);
+    } else {
+      visible[i][j] = true;
+      this.setState({
+        visible: visible,
+      })
     }
-    visible[i][j] = true;
-    this.setState({
-      squares: squares,
-      visible: visible,
-    })
+    this.countRevealed(this.state.visible);
   }
 
   resetGame() {
     const squares = new Array(this.state.row).fill(0);
     const visible = new Array(this.state.row).fill(null);
     const board = generateBoard(squares, visible, this.state.row, this.state.col, this.state.bombCount);
-
     this.setState({
-      isGameOver : false,
-      squares : board[0],
-      visible : board[1],
+      isGameOver: false,
+      isGameCleared: false,
+      revealedCount: 0,
+      squares: board[0],
+      visible: board[1],
     });
   }
 
@@ -170,25 +219,29 @@ class Game extends React.Component {
     let status = '';
     let reset = '';
 
-    if (this.state.isGameOver) {
-      status = "GAME OVER";
+    if (this.state.isGameOver || this.state.isGameCleared) {
+      status = this.state.isGameOver ? "GAME OVER" : "GAME CLEARED";
       reset = <button onClick={() => this.resetGame()}>{"Restart Game"}</button>;
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={this.state.squares}
-            visible={this.state.visible}
-            row={this.state.row}
-            col={this.state.col}
-            onClick={(i, j) => this.handleClick(i, j)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          {reset}
+      <div>
+        <Header />
+        <div className="game container">
+          <div className="game-board">
+            <Board
+              squares={this.state.squares}
+              visible={this.state.visible}
+              row={this.state.row}
+              col={this.state.col}
+              onClick={(i, j) => this.handleClick(i, j)}
+            />
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            {reset}
+            <div> Opened Cells : {this.state.revealedCount}</div>
+          </div>
         </div>
       </div>
     );
